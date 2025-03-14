dec53524dd59e42b9483b9c89f85cbef7cd53153
ThomasUsesAI <thomas.adriaanse.aws@gmail.com>
2025-03-13 20:40:59 -0700

    Add setup.py for development installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quantum-rogue",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "pygame>=2.5.2",
+    ],
+)
